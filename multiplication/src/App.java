0662a3a940ad83184a7642d2772307de349717cf
Aleisson <caioaleisson@gmail.com>
2024-08-21 20:50:52 -0300

    feat: multiplication exercise

---
+++ multiplication/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number = 3;
+
+        for(int i = 0; i <= 10; i++){
+            String menssagem = "3 * " + i + " = " + (i * 3); 
+            System.out.println(menssagem);
+        }
+    }
+}
